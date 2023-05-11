a98c0ce5a5258909b0d8d77e06416239ef8587b4
id2k1149 <id2k1149@gmail.com>
2023-05-11 12:36:52 -0400

    Initial Commit

---
+++ TabBarRotatedIcon/TabBarRotatedIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarRotatedIconApp.swift
+//  TabBarRotatedIcon
+//
+//  Created by Max Franz Immelmann on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TabBarRotatedIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
